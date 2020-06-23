f96155434ecdd44f4ad1f5b4d66a23130696c8a7
Gary Watson <Gary_Watson@Blueyonder.co.uk>
2020-06-23 11:47:34 +0100

    basic arrow shape, click on the rectangle to vary the size

--- Arrow/ContentView.swift
+++ Arrow/ContentView.swift
@@ -8,9 +8,51 @@
 
 import SwiftUI
 
+struct Rectangle: Shape {
+    var insetAmount: CGFloat
+    
+    var animatableData: CGFloat {
+        get { insetAmount }
+        set { self.insetAmount = newValue }
+    }
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: insetAmount, y:rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.maxY))
+        
+        return path
+    }
+}
+
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: rect.maxX / 2, y:rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX / 2, y: rect.minY))
+        
+        return path
+    }
+}
 struct ContentView: View {
+    @State private var insetAmount: CGFloat = 50
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Triangle()
+                .frame(width: 200, height: 200)
+            
+            Rectangle(insetAmount: insetAmount)
+                .frame(width: 200, height: 300)
+                .onTapGesture {
+                    withAnimation {
+                        self.insetAmount = CGFloat.random(in: 10...90)
+                    }
+            }
+        }
     }
 }
 
